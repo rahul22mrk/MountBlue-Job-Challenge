06b82097bb44ed26d7cddd08aa4cf34ce2038d70
Rahul Gupta <59743796+rahul22mrk@users.noreply.github.com>
2022-12-26 13:27:33 +0530

    Create FairRations.java

---
+++ FairRations.java
@@ -0,0 +1,26 @@
+public static String fairRations(List<Integer> b) {
+    // Write your code here
+        int odd=0;
+        for(int i=b.size()-1;i>=0;i--)
+        {
+            if(b.get(i)%2==1)
+            {
+                odd++;
+            }
+        }
+        
+        if(odd%2==1)
+            return "NO";
+        int loaf=0;
+        for(int i=b.size()-1;i>0;i--)
+        {
+            if(b.get(i)%2==1)
+            {
+                b.set(i,b.get(i)+1);
+                b.set(i-1,b.get(i-1)+1);
+                loaf+=2;
+                
+            }
+        }
+        String res=""+loaf;
+        return res;
